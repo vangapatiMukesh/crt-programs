f7436943d75e941a2853c9353714bfbe5a5f9f98
vangapatiMukesh <58410459+vangapatiMukesh@users.noreply.github.com>
2019-12-06 12:44:40 +0530

    Add files via upload

---
+++ Untitled17.cpp
@@ -0,0 +1,18 @@
+//find out given char is uppercase or lower case
+//or digits or special char
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%c",&ch);
+	if(ch>=65&&ch<=90)
+	printf("upper case alphabet");
+	else if (ch>=97&&ch<=122)
+	printf("lower case alphabet");
+	else if(ch>=48&&ch<=57)
+	printf("it is digt");
+	else
+	printf("it is a special character");
+	return 0;
+}
+
---
+++ ascii values.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+
+int a= 15;
+print("%0",a);
+return 0;
+}
