7fc073b0e0cd6163c78a208c555279b87286ff8c
vangapatiMukesh <58410459+vangapatiMukesh@users.noreply.github.com>
2019-12-09 11:20:12 +0530

    Add files via upload

---
+++ rootof digit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+   int n,r,sum=0;
+   scanf("%d",&n);
+   a:
+   while(n!=0)
+   {
+       r=n%10;
+       sum=sum+r;
+       n=n/10;
+   }
+   if(sum>9)
+   {
+       n=sum;
+       sum=0;
+       goto a;
+   }
+   printf("%d",sum);
+   return 0;
+}
